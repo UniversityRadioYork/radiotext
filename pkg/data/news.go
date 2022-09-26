1baf08dff08cab555b9d01fd9629e7e017e578d5
michael-grace <michael.dp.grace@outlook.com>
2022-09-26 19:30:42 +0100

    change numbers to words

--- pkg/data/news.go
+++ pkg/data/news.go
@@ -5,6 +5,21 @@ import (
 	"time"
 )
 
+var hourNumsToWords = map[int]string{
+	0:  "Twelve", // 12 % 12 = 0
+	1:  "One",
+	2:  "Two",
+	3:  "Three",
+	4:  "Four",
+	5:  "Five",
+	6:  "Six",
+	7:  "Seven",
+	8:  "Eight",
+	9:  "Nine",
+	10: "Ten",
+	11: "Eleven",
+}
+
 func (s *RadiotextSession) URYNewsHandler() {
 	// we'll wait until we're less than 2 mins past
 	// then send a high priority message of URY news
@@ -18,7 +33,7 @@ func (s *RadiotextSession) URYNewsHandler() {
 				isNews = true
 				s.PriorityWriteLock = true
 				s.OutputRadioTextMessage(
-					fmt.Sprintf("URY News at %v", now.Format("3")), true)
+					fmt.Sprintf("URY News at %v", hourNumsToWords[now.Hour()%12]), true)
 			}
 		} else {
 			if now.Minute() >= 2 {
